d7ff91d19cc7caf936e53f136096b54518157478
Lisa Reinhardt <reinal@gmail.com>
2018-05-13 16:08:45 -0500

    refactored code to include mongoose

--- server.js
+++ server.js
@@ -4,100 +4,124 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const {PORT, DATABASE_URL} = require('./config');
 const {BlogPosts} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
 app.use(morgan('common'));
+app.use(express.json());
 
 app.get('/blog-posts', (req, res) => {
-    res.json(BlogPosts.get());
+    BlogPosts
+    .find()
+    .then(posts => {
+        res.json(posts.map(post => post.serialize()));
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({error: 'Internal server error'});
+    });
+});
+
+app.get('/blog-posts/:id', (req, res) => {
+    BlogPosts
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error'});
+    });
+});
+
+app.post('/blog-posts', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\`in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    BlogPosts
+    .create({
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author
+    })
+    .then(blogPosts => res.status(201).json(blogPosts.serialize()))
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'There is an error'})
+    });
+});
+
+app.delete('/blog-posts/:id', (req, res) => {
+    BlogPosts
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+        res.status(204).json({message: 'Success!!'});
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'There is an error'});
+    });
+});
+
+app.delete('/:id', (req, res) => {
+    BlogPosts
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+        console.log(`Deleted blog post with id \`${req.params.id}\``);
+        res.status(204).end();
+    });
+});
+
+app.use('*', function(req, res) {
+    res.status(404).json({ message: 'Not Found'});
 });
 
 let server;
 
-function runServer() {
-    const port = process.env.PORT || 8080;
+function runServer(databaseUrl, port=PORT) {
     return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve(server);
-        }).on('error', err => {
-            reject(err)
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+            .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
         });
     });
 }
 
 function closeServer() {
-    return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve();
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
         });
     });
 }
 
 if (require.main === module) {
-    runServer().catch(err => console.error(err));
+runServer(DATABASE_URL).catch(err => console.error(err));
 };
 
 module.exports = {app, runServer, closeServer};
-
-BlogPosts.create('Mauris Dictum Facilisis Augue', `'Donec iaculis gravida nulla. Etiam ligula pede, sagittis 
-	quis, interdum ultricies, scelerisque eu. Aenean vel massa quis mauris vehicula lacinia. Mauris 
-	tincidunt sem sed arcu. Mauris dictum facilisis augue. Donec iaculis gravida nulla. Cum sociis natoque 
-	penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam dapibus fermentum ipsum. 
-    Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Pellentesque arcu.'`, 'Author Three', 'April 2, 2018');
-
-app.post('/blog-posts', jsonParser, (req, res) => {
-    const requiredFields = ['title', 'content', 'author', 'publishDate'];
-    for (let i=0; i<requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`
-            console.error(message);
-            return res.status(400).send(message);
-        }
-    }
-
-const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-    res.status(201).json(item);
-});
-
-app.put('/blog-posts/:id', jsonParser, (req, res) => {
-    const requiredFields = ['title', 'content', 'author', 'publishDate'];
-    for (let i=0; i<requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing \`${field}\` in request body`
-            console.error(message);
-            return res.status(400).send(message);
-        }
-    }
-
-    if (req.params.id !== req.body.id) {
-        const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-        console.error(message);
-        return res.status(400).send(message);
-    }
-    console.log(`Updating blog post \`${req.params.id}\``);
-    BlogPosts.update({
-        id: req.params.id,
-        title: req.body.title,
-        content: req.body.content,
-        author: req.body.author, 
-        publishDate: req.body.publishDate
-      });
-      res.status(204).end();
-});
-
-app.delete('/blog-posts/:id', (req, res) => {
-    BlogPosts.delete(req.params.id);
-    console.log(`Deleted blog post \`${req.params.ID}\``);
-    res.status(204).end();
-});
--- test/test-blog.js
+++ test/test-blog.js
@@ -36,11 +36,11 @@ describe('BlogPosts', function() {
         .send(newPost)
         .then(function(res) {
             expect(res).to.have.status(201);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
-        expect(res.body.id).to.not.equal(null);
-        expect(res.body).to.deep.equal(Object.assign(newPost, {id: res.body.id}));
+            expect(res).to.be.json;
+            expect(res.body).to.be.a('object');
+            expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
+            expect(res.body.id).to.not.equal(null);
+            expect(res.body).to.deep.equal(Object.assign(newPost, {id: res.body.id}));
         });
     });
 
