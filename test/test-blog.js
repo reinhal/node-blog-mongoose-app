b96c0afc804fb221ecc1dab5de9e127090c0ca4e
Lisa Reinhardt <reinal@gmail.com>
2018-05-12 20:31:17 -0500

    updated test.js file Merge branch 'master' of https://github.com/reinhal/jusnoshin

--- test/test-blog.js
+++ test/test-blog.js
@@ -29,6 +29,38 @@ describe('BlogPosts', function() {
         });
     });
 
+    it('should add a new blog post on POST', function() {
+        const newPost = {title: 'Salami Magna Ribeye', content: 'In picanha pork. Aliqua strip steak hamburger irure occaecat est. Aliquip frankfurter turkey qui dolor. Labore pastrami corned beef frankfurter tail excepteur eu. Qui rump beef ribeye jerky nostrud. Short loin veniam irure ribeye capicola pastrami. Venison meatball ut exercitation, irure pork chop enim.', author: 'Bresaola Dolore', publishDate: 'May, 4, 2018'}
+        return chai.request(app)
+        .post('/blog-posts')
+        .send(newPost)
+        .then(function(res) {
+            expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'title', 'content', 'author', 'publishDate');
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(Object.assign(newPost, {id: res.body.id}));
+        });
+    });
+
+    it('should update posts on PUT', function() {
+        return chai.request(app)
+            .get('/blog-posts')
+            .then(function(res) {
+                const updatedPost = Object.assign(res.body[0], {
+                    title: 'Spicy Jalapeno',
+					content: 'Filet mignon biltong andouille pork chop pig t-bone, labore salami non esse. Ex fugiat nisi brisket, ball tip adipisicing esse rump dolor ea enim hamburger aute salami. Officia lorem doner rump ut drumstick exercitation do pancetta kevin enim. Brisket buffalo qui et.',  
+                });
+                return chai.request(app)
+                .put(`/blog-posts/${res.body[0].id}`)
+                .send(updatedPost)
+                .then(function(res) {
+                    expect(res).to.have.status(204);
+                });
+            });
+    });
+
     it('should delete posts on DELETE', function() {
         return chai.request(app)
         .get('/blog-posts')
